3de913f6a089bb33e181a65f2d71a829ea19fee1
messified <jesse@koleimports.com>
2013-05-10 14:43:59 -0700

    updated files with working code

--- client.php
+++ client.php
@@ -1,8 +1,27 @@
 <?php
-require_once 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
+
+//Error Handling
+ini_set('display_errors', 'On');
 
 use Application\DropshipProductClient;
 
-$dropshipClient = new DropshipProductClient();
-//Get all products
-$productList = $dropshipClient->getProductList();
+$DropshipProductClient = new DropshipProductClient();
+
+$productList = $DropshipProductClient->connectAPI();
+//Get Request
+$request = $productList->get('products');
+
+//Error Handling
+try {
+	$command = $productList->getCommand('GetProducts');
+	$result = $command->execute();
+	print_r($result);
+} catch (Guzzle\Http\Exception\BadResponseException $e) {
+    echo 'Uh oh! ' . $e->getMessage();
+    echo 'HTTP request URL: ' . $e->getRequest()->getUrl() . "\n";
+    echo 'HTTP request: ' . $e->getRequest() . "\n";
+    echo 'HTTP response status: ' . $e->getResponse()->getStatusCode() . "\n";
+    echo 'HTTP response: ' . $e->getResponse() . "\n";
+}
+
--- src/Application/DropshipProductClient.php
+++ src/Application/DropshipProductClient.php
@@ -1,42 +1,32 @@
 <?php
 namespace Application;
 
+require(dirname(dirname(dirname(__FILE__))).'/vendor/autoload.php');
+
 use Guzzle\Service\Client;
+use Guzzle\Common\Collection;
+use Guzzle\Service\Builder\ServiceBuilder;
 use Guzzle\Service\Description\ServiceDescription;
+use Guzzle\Plugin\CurlAuth\CurlAuthPlugin;
 use Guzzle\Http\Message\Response;
 
 class DropshipProductClient extends Client
 {
-	public static function factory($config = array())
+	public function connectAPI()
 	{
-		$default = array(
-		'base_url' => 'https://api.koleimports.com/'
-	);
+		//Client Setup
+		$client = new Client('https://api.koleimports.com/', array(
+			'curl.options'	=> array(
+			CURLOPT_HTTPAUTH		=> 'CURLAUTH_BASIC',
+			CURLOPT_USERPWD		=> 'X01003:ae25bfd04c13438a17914ce258ff1b1c25ee9e12',
+			CURLOPT_RETURNTRANSFER	=> 'true'
+			)
+		));
 
-	$required = array('base_url',
-		array('curl.options' 					=> array(
-		CURLOPT_HTTPAUTH   				=> 'CURLAUTH_BASIC',
-		CURLOPT_USERPWD    				=> 'X01003:ae25bfd04c13438a17914ce258ff1b1c25ee9e12',
-		CURLOPT_RETURNTRANSFER   		=> 'true'
-		)
-	));
+		//Attach a service description to the client
+		$description = ServiceDescription::factory(__DIR__ .'/services.json');
+		$client->setDescription($description);
 
-	$config = Collection::fromConfig($config, $default, $required);
-	$client = new self($config->get('base_url'), $config);
-
-	// Attach a service description to the client
-	$client->setDescription(ServiceDescription::factory(dirname(dirname(__FILE__)).'/Services/dropship_products.json'));
-	return $client;
-	}
-
-	public function getProductList()
-	{
-		/**Create GetProducts command
-		$command = $client->getCommand('GetProducts');
-		//Execute the command to retrieve the product model object
-		$result = $command->execute();
-		return $result;**/
-		echo 'Time to pack it in!';
+		return $client;
 	}
 }
-
