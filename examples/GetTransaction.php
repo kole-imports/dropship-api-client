01eb7a3ffad0f6041094c50694732471877dbc71
messified <jesse.c.reese@gmail.com>
2013-06-12 16:13:19 +0000

    added more examples

---
+++ examples/GetTransaction.php
@@ -0,0 +1,21 @@
+<?php
+
+$vendorDir = dirname(dirname(__FILE__));
+require($vendorDir . '/vendor/autoload.php');
+
+//Error Handling
+ini_set('display_errors', 'On');
+
+use KoleImports\DropshipApi\Service\ServiceBuilder;
+use KoleImports\DropshipApi\Model\Request\Order;
+
+$serviceBuilder = new ServiceBuilder('X16310', 'a0f0e69913896e20bdb07a9c31d9d7f1d31e3acd');
+
+$transactionService = $serviceBuilder->getTransactionService();
+
+$order = new Order;
+$order->setOrderId('12345');
+
+$response = $transactionService->getTransaction($order->getOrderId());
+
+var_dump($response);
