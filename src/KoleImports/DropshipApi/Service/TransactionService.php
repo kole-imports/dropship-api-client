5f15d6633847ef886cbbea2df6ca1ea02cf5d9db
messified <jesse.c.reese@gmail.com>
2013-06-13 23:59:49 +0000

    fixed bug with Transaction service

--- src/KoleImports/DropshipApi/Service/TransactionService.php
+++ src/KoleImports/DropshipApi/Service/TransactionService.php
@@ -19,6 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 namespace KoleImports\DropshipApi\Service;
 
+use KoleImports\DropshipApi\Model\Request\Order;
+
 class TransactionService
 {
     private $client;
