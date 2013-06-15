8d7eb95932770fb2814969dc8c3f5fd54c20e7b7
Jesse Reese <jesse.c.reese@gmail.com>
2013-06-15 12:31:02 -0600

    Update GetOrder.php

--- examples/GetOrder.php
+++ examples/GetOrder.php
@@ -7,7 +7,6 @@ require($vendorDir . '/vendor/autoload.php');
 ini_set('display_errors', 'On');
 
 use KoleImports\DropshipApi\Service\ServiceBuilder;
-use KoleImports\DropshipApi\Model\Request\Order;
 
 $serviceBuilder = new ServiceBuilder('X16310', 'a0f0e69913896e20bdb07a9c31d9d7f1d31e3acd');
 
