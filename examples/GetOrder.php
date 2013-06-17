06f2e72bfc3a1f3266fa28d027380b3fa9839eb1
Jesse Reese <jesse.c.reese@gmail.com>
2013-06-17 12:14:35 -0600

    Update GetOrder.php

--- examples/GetOrder.php
+++ examples/GetOrder.php
@@ -8,7 +8,7 @@ ini_set('display_errors', 'On');
 
 use KoleImports\DropshipApi\Service\ServiceBuilder;
 
-$serviceBuilder = new ServiceBuilder('X16310', 'a0f0e69913896e20bdb07a9c31d9d7f1d31e3acd');
+$serviceBuilder = new ServiceBuilder('ACCOUNT ID', 'API KEY');
 
 $orderService = $serviceBuilder->getOrderService();
 
